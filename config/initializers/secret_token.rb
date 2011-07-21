f8ea00938f6189f599f169c7a6f101b0bb30e0b8
tatygrassini <tatygrassini@gmail.com>
2011-07-21 21:36:52 +0200

    Skeleton ready to rock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4eb6fc5fe948d368fca59c16546f1ec0cfe975be87ba23173fdf40a97d6ab4ca39d52e40565728ee00930bf8b5456e2d5038057871297f26b937f91780723fe7'
